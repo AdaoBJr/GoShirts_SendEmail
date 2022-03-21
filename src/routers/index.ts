87cdb14a0036c90d741487dba2a6c177a2eeab0d
AdaoBJr <adaobjr@gmail.com>
2022-03-21 11:17:13 -0400

    feat: :fire: removed CreateUser

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { createUserRouter } from './createUser.routes';
 import { passwordRouter } from './password.routes';
 
 const router = Router();
 
-router.use('/register', createUserRouter);
 router.use('/password', passwordRouter);
 
 export { router };
