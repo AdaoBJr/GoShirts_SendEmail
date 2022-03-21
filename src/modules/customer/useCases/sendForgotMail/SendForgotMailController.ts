aec9dd53546c4103293c1d7eaf8e4e8660028fab
AdaoBJr <adaobjr@gmail.com>
2022-03-21 18:03:48 -0400

    feat: :sparkles: added parseRequestBody on SendForgotMailController

--- src/modules/customer/useCases/sendForgotMail/SendForgotMailController.ts
+++ src/modules/customer/useCases/sendForgotMail/SendForgotMailController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
+import { parseRequestBody } from 'src/utils/parseRequestBody';
 import { SendForgotMailUseCase } from './SendForgotMailUseCase';
 
 class SendForgotMailController {
   constructor(private sendForgotMailUseCase: SendForgotMailUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
-    if (!request.body.data) throw new Error(`User does not exists!`);
-    const { data } = request.body;
+    const data = parseRequestBody(request);
 
     await this.sendForgotMailUseCase.execute(data);
-    return response.status(200).send();
+    return response.status(200).json({ sent: true });
   }
 }
 
