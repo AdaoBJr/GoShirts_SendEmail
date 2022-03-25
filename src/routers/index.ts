3c5d4d26595de25495f388516bd0a44cf86d18c4
AdaoBJr <adaobjr@gmail.com>
2022-03-24 22:57:17 -0400

    feat: ✨ created resetRouters on Router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { passwordRouter } from './password.routes';
+import { resetPwdRouter } from './resetPwd.routes';
 
 const router = Router();
 
 router.use('/password', passwordRouter);
+router.use('/password', resetPwdRouter);
 
 export { router };
