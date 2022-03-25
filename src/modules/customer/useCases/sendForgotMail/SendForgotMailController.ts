e34826019d29bd56a5efb26b8e412740393213b0
AdaoBJr <adaobjr@gmail.com>
2022-03-24 22:54:33 -0400

    feat: ✨ updated SendForgotMailController

--- src/modules/customer/useCases/sendForgotMail/SendForgotMailController.ts
+++ src/modules/customer/useCases/sendForgotMail/SendForgotMailController.ts
@@ -6,9 +6,9 @@ class SendForgotMailController {
   constructor(private sendForgotMailUseCase: SendForgotMailUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
-    const data = parseRequestBody(request);
+    const { token, body } = parseRequestBody(request);
 
-    await this.sendForgotMailUseCase.execute(data);
+    await this.sendForgotMailUseCase.execute({ token, ...body });
     return response.status(200).json({ sent: true });
   }
 }
