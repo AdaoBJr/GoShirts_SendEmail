7f5ce72c682b52b0770d0e3a101fd2932c67533a
AdaoBJr <adaobjr@gmail.com>
2022-03-27 22:32:33 -0400

    feat: ✨ created subscribeNewslleter on customer.gql

--- src/providers/GoShirtsProvider/gql/customer.gql.ts
+++ src/providers/GoShirtsProvider/gql/customer.gql.ts
@@ -1,9 +1,9 @@
 import { gql } from 'graphql-request';
 
-export const CHANGE_PASSWORD_MUTATION = gql`
-  mutation changePwdCustomer($userId: String!, $newPassword: String!) {
-    changePassword(data: { userId: $userId, newPassword: $newPassword }) {
-      newPassword
+export const SUBSCRIBE_NEWSLETTER_MUTATION = gql`
+  mutation subscribeNewsletter($email: String!) {
+    subscribeNewsletter(email: $email) {
+      subscribed
     }
   }
 `;
